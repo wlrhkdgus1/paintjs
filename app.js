ca48f4f613d28f8f6da114f5b7856fbe20c75039
Gwang Hyun <gwgw4578@gmail.com>
2021-03-10 23:16:08 +0900

    21 03 10

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d"); // context는 canvas 안에서 픽셀을 다루는것.
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -36,6 +39,21 @@ function handleColorClick(event){
     ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "FILL";
+    } else {
+        filling = true;
+        mode.innerText = "PAINT";
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove); // 캔버스안에서
     canvas.addEventListener("mousedown", startPainting); // 캔버스안에서 클릭시
@@ -45,3 +63,11 @@ if(canvas){
 
 Array.from(colors).forEach(color => 
     color.addEventListener("click", handleColorClick));
+
+if(range){
+    range.addEventListener("input",handleRangeChange);
+}    
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
