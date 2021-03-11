bcac90f801dc277d67847713dbfce13b92a8315b
Gwang Hyun <gwgw4578@gmail.com>
2021-03-12 05:11:43 +0900

    21 03 12 paint js finish

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d"); // context는 canvas 안에서 픽셀을 �
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -65,12 +68,26 @@ function handleCanvasClick(){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL(); // default 가 png
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
+
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove); // 캔버스안에서
     canvas.addEventListener("mousedown", startPainting); // 캔버스안에서 클릭시
     canvas.addEventListener("mouseup", stopPainting); // 캔버스안에서 클릭을 해제시
     canvas.addEventListener("mouseleave", stopPainting); // 캔버스 밖으로 나가면
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => 
@@ -82,4 +99,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
